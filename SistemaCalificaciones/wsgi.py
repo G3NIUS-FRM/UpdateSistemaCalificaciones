501eb665ceb09e0de5947dd25c878ae35e90d642
Your Name <you@example.com>
2025-02-27 19:34:52 -0300

    soy retrasado

--- SistemaCalificaciones/wsgi.py
+++ SistemaCalificaciones/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SistemaCalificacion.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SistemaCalificaciones.settings')
 
 application = get_wsgi_application()
